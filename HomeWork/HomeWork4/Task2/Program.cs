f814df2306d1ee5a9ca56fc06fb16d9714cefc22
VeraMaminova <maminovavv@yandex.ru>
2022-08-20 23:04:49 +0300

    Add Task2

---
+++ HomeWork/HomeWork4/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+int SumDigits(int number)
+{
+    int digit;
+    int sum = 0;
+    while (number > 0)
+    {
+        digit = number % 10;
+        number = number / 10;
+        sum = sum + digit;
+    }
+    return sum;
+}
+int number = Prompt($"Введите число   >");
+System.Console.WriteLine($"Сумма цифр в числе равна {SumDigits(number)}");
+SumDigits(number);
+
