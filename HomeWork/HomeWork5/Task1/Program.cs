bbc02bcadd4aae8516a87bf46a49f71209d0230a
VeraMaminova <maminovavv@yandex.ru>
2022-08-24 21:33:24 +0300

    Add Task1

---
+++ HomeWork/HomeWork5/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+int[] CreateArray(int length)
+{
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = new Random().Next(99, 1000);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int EvenNumbered(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int sizeMassiv = Prompt("Введите размер массива >");
+int[] newMassiv = CreateArray(sizeMassiv);
+PrintArray(newMassiv);
+System.Console.WriteLine(EvenNumbered(newMassiv));
