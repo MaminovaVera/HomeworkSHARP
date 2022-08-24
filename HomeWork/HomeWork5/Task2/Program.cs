922ef45a1bcf2502d82f0a54d0a99048985504ac
VeraMaminova <maminovavv@yandex.ru>
2022-08-24 21:53:20 +0300

    Add Task2

---
+++ HomeWork/HomeWork5/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+int[] CreateArray(int length)
+{
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = new Random().Next(0, 10);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array) // вывод массива на консоль
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int SumOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+int sizeMassiv = Prompt("Введите размер массива >");
+int[] newMassiv = CreateArray(sizeMassiv);
+PrintArray(newMassiv);
+System.Console.WriteLine(SumOddPosition(newMassiv));
