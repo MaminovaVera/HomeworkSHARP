7b3599a2f4c6e7c4bb40bad3cfb2c605ca95dce2
VeraMaminova <maminovavv@yandex.ru>
2022-09-04 22:19:58 +0300

    Add HW8

---
+++ HomeWork/HomeWork8/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                    // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
+
+int[,] FillArray(int numLine, int numColumns)         // Функция создания и заполнения двумерного массива
+{
+    int[,] matrix = new int[numLine, numColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] OrderingInDescendingOrder(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1 - k; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+
+int[,] matrix = FillArray(Prompt("Введите число строк > "), Prompt("Введите число столбцов > "));
+PrintArray(matrix);
+System.Console.WriteLine();
+int[,] result=OrderingInDescendingOrder(matrix);
+PrintArray(result);
